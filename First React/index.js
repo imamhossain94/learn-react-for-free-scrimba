0f071e98fc6af8615adc40f49bb0f97684107284
imammasleap <imam@masleap.io>
2022-05-20 13:54:10 +0600

    feat: First react CDN and Babel

---
+++ First React/index.js
@@ -0,0 +1,4 @@
+
+ReactDOM.render(<h1>First React</h1>, document.getElementById("root"))
+
+document.getElementById("htag").innerHTML = "Hello World!"
--- First/index.js
+++
@@ -1,4 +0,0 @@
-
-ReactDOM.render(<h1>First React Practice</h1>, document.getElementById("root"))
-
-document.getElementById("htag").innerHTML = "Hello World"
