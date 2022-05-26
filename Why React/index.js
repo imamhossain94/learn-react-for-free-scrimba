2ff6717b898c6df75e1351b311b4638be3dbdf96
imammasleap <imam@masleap.io>
2022-05-26 13:43:17 +0600

    feat: React and bootstrap

---
+++ Why React/index.js
@@ -0,0 +1,81 @@
+
+//react bootstrap nav bar
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="#">Features</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+    );
+}
+
+
+function Table() {
+    return (
+        <table className="table table-dark table-striped">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
+
+
+
+
+
+
+ReactDOM.render(
+    <div>
+        <Navbar />
+        <div>
+            <Table />
+        </div>
+    </div>
+    , document.getElementById("root"))
+
